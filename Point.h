7a832e2a365cafa9ee1c12faf0ef900810b6cd2e
Jeff Muizelaar <jmuizelaar@mozilla.com>
2014-09-08 10:54:26 -0400

    Bug 1049564. Remove sketchy sizing code from CompositorOGL. r=nical
    This came from the layers refactoring and taking it out doesn't
    seem to cause any test failures.

--- Point.h
+++ Point.h
@@ -92,6 +92,7 @@ struct PointTyped :
   }
 };
 typedef PointTyped<UnknownUnits> Point;
+static_assert(sizeof(Point) == 2*sizeof(Float));
 
 template<class units>
 IntPointTyped<units> RoundedToInt(const PointTyped<units>& aPoint) {
